b999ecf24da52e28fc85ea6e75f40aa9a5c47e2b
RS_ict <r.stefani87.informatica@gmail.com>
2025-03-19 01:34:11 +0100

    fi:

--- service-account-dev.js
+++ service-account-dev.js
@@ -1,12 +1,21 @@
 /** 
 * @author Roberto Stefani 
 **/ 
-import {encrypt} from '@ares/core/security.js';
+import {encrypt, decrypt} from '@ares/core/security.js';
 import {getMD5Hash} from '@ares/core/crypto.js';
 import {getFileContent, setFileContent} from '@ares/files';
   const k59870WWW = getMD5Hash('k59870WWW');
+
  export function encryptGoogleServiceAccountKey(absKeyFilePath, password) {
   const file = JSON.parse(getFileContent(absKeyFilePath));
-  const encryptedFile = encryptObject(file, `${password}${k59870WWW}`);
+  const encryptedFile = encrypt(file, `${password}${k59870WWW}`);
   setFileContent(absKeyFilePath, JSON.stringify(encryptedFile));
+ }
+
+ export function decryptGoogleServiceAccountKey(absKeyFilePath, password) {
+  const file = JSON.parse(getFileContent(absKeyFilePath));
+  console.debug('file', file);
+  console.debug('password', password);
+  const decryptedFile = decrypt(file, `${password}${k59870WWW}`);
+  setFileContent(absKeyFilePath, JSON.stringify(decryptedFile));
  }
